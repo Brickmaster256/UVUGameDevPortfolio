b190e89c45d5e218660d95993ba0f09b0970f40e
MatthewCamacho <mcamacho2056@gmail.com>
2026-01-15 15:09:52 -0700

    removed teleporting as it isn't really needed, added test interactables

---
+++ GameDevProjects/Assets/VRResturauntSim/Scripts/ActivateTeleportationRay.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
+
+public class ActivateTeleportationRay : MonoBehaviour
+{
+    public GameObject leftTeleportationRay;
+    public GameObject rightTeleportationRay;
+
+    public InputActionProperty leftActivate;
+    public InputActionProperty rightActivate;
+    
+    void Update()
+    {
+        leftTeleportationRay.SetActive(leftActivate.action.ReadValue<float>() > 0.1f);
+        rightTeleportationRay.SetActive(rightActivate.action.ReadValue<float>() > 0.1f);
+    }
+}
