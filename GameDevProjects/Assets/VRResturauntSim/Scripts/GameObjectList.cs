67589b720e208b5eb28d394d14287e0a42e452ed
MatthewCamacho <mcamacho2056@gmail.com>
2026-01-16 17:33:43 -0700

    started adding a tray to detect the food on it

---
+++ GameDevProjects/Assets/VRResturauntSim/Scripts/GameObjectList.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New GameObjectList", menuName = "Scriptable Objects/GameObjectList")]
+public class GameObjectList : ScriptableObject
+{
+    public List<GameObject> gameObjectList;
+
+    public void AddToList(GameObject go)
+    {
+        gameObjectList.Add(go);
+    }
+
+    public void RemoveFromList(GameObject go)
+    {
+        gameObjectList.Remove(go);
+    }
+}
---
+++ GameDevProjects/Assets/VRResturauntSim/Scripts/TriggerGameObjectList.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerGameObjectList : MonoBehaviour
+{
+    public List<GameObject> gameObjectList;
+    
+    
+    
+    public void onTriggerEnter(Collider other)
+    {
+        gameObjectList.Add(other.gameObject);
+    }
+
+    public void onTriggerExit(Collider other)
+    {
+        gameObjectList.Remove(other.gameObject);
+    }
+}
