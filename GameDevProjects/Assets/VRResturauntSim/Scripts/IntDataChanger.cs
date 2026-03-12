0253f9fb1886c2ed4dd2d963bb708dc47173f5cb
MatthewCamacho <142838286+Brickmaster256@users.noreply.github.com>
2026-03-12 14:13:53 -0600

    attempted to add a score tracker

---
+++ GameDevProjects/Assets/VRResturauntSim/Scripts/IntDataChanger.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class IntDataChanger : MonoBehaviour
+{
+    public IntData intdata;
+    public FloatData floatdata;
+    public float multiplier;
+    public float amount;
+    public int amountInt;
+    
+
+    public void IntAddMultipler(IntData data)
+    {
+        data.Value += (int)(intdata.Value * multiplier);
+
+    }
+    public void IntAddMultipler(int data)
+    {
+        intdata.Value += (int)(data * multiplier);
+
+    }
+
+    public void FloatAddMultipler(FloatData data)
+    {
+        data.UpdateValue(intdata.Value * multiplier);
+        Debug.Log(data.Value);
+    }
+    public void FloatAddMultipler(IntData data)
+    {
+        
+        amount = data.Value * multiplier;
+        floatdata.UpdateValue(amount);
+        Debug.Log(floatdata.Value);
+    }
+    
+    public void FloatAddMultipler(float data)
+    {
+        floatdata.Value += (data * multiplier);
+
+    }
+
+   
+    
+}
