1f7d2f9ea83d6cf6702ef21536f91a8304535940
MatthewCamacho <142838286+Brickmaster256@users.noreply.github.com>
2026-04-06 21:14:06 -0600

    removed unused music,  added credits to used music, added a listener for when the game loses focus

---
+++ GameDevProjects/Assets/VRResturauntSim/Scripts/GameLossFocus.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+using Unity.XR.Oculus;
+
+
+public class GameLossFocus : MonoBehaviour
+{
+    public UnityEvent OnGameLossFocus, OnGameRefocus;
+
+    public void OnEnable()
+    {
+        InputFocus.InputFocusAcquired += OnInputFocusAcquired;
+        InputFocus.InputFocusLost += OnInputFocusLost;
+    }
+
+    public void OnDisable()
+    {
+        InputFocus.InputFocusAcquired -= OnInputFocusAcquired;
+        InputFocus.InputFocusLost -= OnInputFocusLost;
+    }
+    
+    
+    public void OnApplicationFocus(bool hasFocus)
+    {
+        if (!hasFocus)
+        {
+            OnGameLossFocus.Invoke();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnInputFocusAcquired()
+    {
+        OnGameRefocus.Invoke();
+    }
+
+    public void OnInputFocusLost()
+    {
+        OnGameLossFocus.Invoke();
+    }
+}
