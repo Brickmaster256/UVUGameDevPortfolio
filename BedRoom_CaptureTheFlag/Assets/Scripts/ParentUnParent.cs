e4ffcf7179c7b8e1bbc89473a11416729be617cf
MatthewCamacho <142838286+Brickmaster256@users.noreply.github.com>
2026-05-31 21:53:28 -0600

    allowed for the flags to be parented to the hands after being instaintiated

--- BedRoom_CaptureTheFlag/Assets/Scripts/ParentUnParent.cs
+++ BedRoom_CaptureTheFlag/Assets/Scripts/ParentUnParent.cs
@@ -2,8 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ParentUnParent : MonoBehaviour
 {
+    public GameActionAdvanced handAction;
+    private GameObject handLocation;
+
+    protected virtual void OnEnable()
+    {
+        if (handAction != null) handAction.Raise += OnRaise;
+        
+    }
+
+    private void OnRaise(object obj)
+    {
+        handLocation = obj as GameObject;
+        
+    }
+    
     public void Unparent(GameObject go)
     {
         go.transform.SetParent(null);
@@ -14,4 +30,19 @@ public class ParentUnParent : MonoBehaviour
         transform.SetParent(go.transform);
         transform.localPosition = Vector3.zero;
     }
+
+    public void ParentWithGameObject()
+    {
+       
+        transform.SetParent(handLocation.transform);
+        transform.localPosition = Vector3.zero;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        transform.SetParent(other.transform);
+        transform.localPosition = Vector3.zero;
+    }
+    
+    
 }
