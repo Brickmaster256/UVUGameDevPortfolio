f6050754709f8073ab98f0aa7d489416d8bb200d
MatthewCamacho <142838286+Brickmaster256@users.noreply.github.com>
2026-06-08 22:23:17 -0600

    added basic models to represent pieces being used

--- BeginnerGameJam/Assets/Scripts/ChessPiece.cs
+++ BeginnerGameJam/Assets/Scripts/ChessPiece.cs
@@ -1,6 +1,32 @@
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
-public class ChessPiece : MonoBehaviour
+public class ChessPiece : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler,
+    IPointerEnterHandler, IPointerExitHandler
 {
-    
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        
+    }
 }
