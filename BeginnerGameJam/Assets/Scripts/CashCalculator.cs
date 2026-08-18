4f761218fc121ebc365302bcd2933d5e76291ea8
MatthewCamacho <142838286+Brickmaster256@users.noreply.github.com>
2026-08-17 20:54:04 -0600

    added methods to allow for the multiplier to change

--- BeginnerGameJam/Assets/Scripts/CashCalculator.cs
+++ BeginnerGameJam/Assets/Scripts/CashCalculator.cs
@@ -8,9 +8,23 @@ public class CashCalculator : MonoBehaviour
     [SerializeField] private IntData grassHealth;
     [SerializeField] private IntData cash;
     [SerializeField] private float cashMultpiplier;
-    
-    
 
+
+    public void SetMultpiplier(float multpiplier)
+    {
+        cashMultpiplier = multpiplier;
+    }
+
+    public float GetMultpiplier()
+    {
+        return cashMultpiplier;
+    }
+
+    public void updateMultpiplier(float amount)
+    {
+        cashMultpiplier += amount;
+    }
+    
     public void calculate()
     {
         cash.Value += (int)(grassHealth.Value * cashMultpiplier); 
