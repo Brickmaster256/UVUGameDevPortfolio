03b488c943be64471e3417fae678d9be421a6642
MatthewCamacho <142838286+Brickmaster256@users.noreply.github.com>
2026-08-18 11:02:18 -0600

    created a script to connect the UI buttons to the network server

--- BeginnerGameJam/Assets/Scripts/Multiplayer/GameManager.cs
+++ BeginnerGameJam/Assets/Scripts/Multiplayer/GameManager.cs
@@ -3,5 +3,33 @@ using UnityEngine;
 
 public class GameManager : NetworkBehaviour
 {
-    
+    [SerializeField] private MultiplayerUI multiplayerUI;
+
+    private void Start()
+    {
+        if (multiplayerUI != null)
+        {
+            multiplayerUI.OnStartHost += StartHost;
+            multiplayerUI.OnStartClient += StartClient;
+            multiplayerUI.OnDiconnectClient += DisconnectClient;
+        }
+    }
+
+    public void StartHost()
+    {
+        multiplayerUI.DisableButtons();
+        NetworkManager.StartHost();
+    }
+
+    public void StartClient()
+    {
+        multiplayerUI.DisableButtons();
+        NetworkManager.StartClient();
+    }
+
+    public void DisconnectClient()
+    {
+        multiplayerUI.DisableButtons();
+        NetworkManager.Shutdown();
+    }
 }
