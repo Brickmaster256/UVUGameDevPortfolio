5650ad746397d9a5d5666fa20b0a550bf0b73492
MatthewCamacho <142838286+Brickmaster256@users.noreply.github.com>
2026-08-17 20:52:02 -0600

    created a simple script that will calculate the amount of money the player will recieve for each activation of the mower

---
+++ BeginnerGameJam/Assets/Scripts/CashCalculator.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CashCalculator : MonoBehaviour
+{
+    public UnityEvent OnCashChanged;
+    
+    [SerializeField] private IntData grassHealth;
+    [SerializeField] private IntData cash;
+    [SerializeField] private float cashMultpiplier;
+    
+    
+
+    public void calculate()
+    {
+        cash.Value += (int)(grassHealth.Value * cashMultpiplier); 
+        OnCashChanged.Invoke();
+        
+    }
+    
+    
+}
